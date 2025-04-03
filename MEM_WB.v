bf21c17317b3ef8273b5b0969eb17a2b062c137f
cwpeck2 <161093366+cwpeck2@users.noreply.github.com>
2025-04-02 19:02:05 -0500

    Update MEM_WB.v

--- MEM_WB.v
+++ MEM_WB.v
@@ -1,4 +1,4 @@
-module MEM_WB (clk, rst_n, in_ALU_result, in_memData, in_MemToReg, in_RegWrite, in_HLT, in_DstReg, out_ALU_result, out_DstReg, out_HLT, out_MemToReg, out_RegWrite,	out_memData);
+module MEM_WB (clk, rst_n, in_ALU_result, in_memData, in_MemToReg, in_RegWrite, in_HLT, in_DstReg, in_PCToReg, out_ALU_result, out_DstReg, out_HLT, out_MemToReg, out_RegWrite,	out_memData);
 
 input clk;
 input rst_n;
@@ -10,7 +10,7 @@ input in_MemToReg;				// mem data and alu result mux
 input in_RegWrite;				// write enable for WB
 input in_HLT;					// halt
 input[3:0] in_DstReg;			// dest register for WB
-
+input in_PCToReg;
 // Outputs to WB stage
 output[15:0] out_ALU_result;	// alu output
 output[15:0] out_memData;		// mem data for load instr
@@ -18,11 +18,12 @@ output out_MemToReg;			// WB source
 output out_RegWrite;			// write enable for reg file
 output out_HLT;					// halt out
 output[3:0] out_DstReg;			// output for dest reg WB
-
+output out_PCToReg;
 // dffs for each signal
 dff ALU_out_reg[15:0](.q(out_ALU_result), .d(in_ALU_result), .wen(1'b1), .clk(clk), .rst(~rst_n));
 dff memData_reg[15:0](.q(out_memData), .d(in_memData), .wen(1'b1), .clk(clk), .rst(~rst_n));
 dff memToReg_reg(.q(out_MemToReg), .d(in_MemToReg), .wen(1'b1), .clk(clk), .rst(~rst_n));
+dff regWrite_reg(.q(out_RegWrite), .d)(in_RegWrite), .wen(1'b1), .clk(clk), .rst(~rst_n));
 dff regWrite_reg(.q(out_RegWrite), .d(in_RegWrite), .wen(1'b1), .clk(clk), .rst(~rst_n));
 dff hlt_reg(.q(out_HLT), .d(in_HLT), .wen(1'b1), .clk(clk), .rst(~rst_n));
 dff dstReg_reg[3:0](.q(out_DstReg), .d(in_DstReg), .wen(1'b1), .clk(clk), .rst(~rst_n));
